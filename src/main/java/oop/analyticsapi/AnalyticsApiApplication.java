f4da21294fe848a707bae70c0d452ce241bd1776
sticker99 <srikar.primary@gmail.com>
2023-10-28 14:58:02 +0800

    Registerd service as portfolio-management in eureka registry
    Signed-off-by: sticker99 <srikar.primary@gmail.com>

--- src/main/java/oop/analyticsapi/AnalyticsApiApplication.java
+++ src/main/java/oop/analyticsapi/AnalyticsApiApplication.java
@@ -2,9 +2,11 @@ package oop.analyticsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AnalyticsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyticsApiApplication.class, args);
