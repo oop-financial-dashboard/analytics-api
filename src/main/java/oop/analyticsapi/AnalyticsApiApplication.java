ac9f020fa9bff0811389b9a2f7e32485a9128b6a
bry4ncheng <chengb21799@gmail.com>
2023-11-06 23:29:56 +0800

    Add back eureka config

--- src/main/java/oop/analyticsapi/AnalyticsApiApplication.java
+++ src/main/java/oop/analyticsapi/AnalyticsApiApplication.java
@@ -2,12 +2,12 @@ package oop.analyticsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableScheduling
 public class AnalyticsApiApplication {
 	public static void main(String[] args) {
